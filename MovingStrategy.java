d9b3083ae8db683cdb45021722da84fc2ff1df1c
Anshuman Mishra <82168547+anshuman2166AppproDev@users.noreply.github.com>
2021-07-07 18:06:39 +0530

    Add files via upload

---
+++ MovingStrategy.java
@@ -0,0 +1,5 @@
+package com.KAB.ParticleTextViewAix;
+
+public abstract class MovingStrategy {
+    public abstract void setMovingPath(Particle particle, int rangeWidth, int rangeHeight, double[] targetPosition);
+}
---
+++ ParticleTextViewAix.java
@@ -0,0 +1,83 @@
+package com.KAB.ParticleTextViewAix;
+
+import android.app.Activity;
+import android.content.Context;
+import com.google.appinventor.components.annotations.*;
+import com.google.appinventor.components.common.ComponentCategory;
+import com.google.appinventor.components.runtime.AndroidNonvisibleComponent;
+import com.google.appinventor.components.runtime.ComponentContainer;
+import com.google.appinventor.components.runtime.EventDispatcher;
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import com.google.appinventor.components.annotations.*;
+import android.widget.FrameLayout;
+import com.google.appinventor.components.common.ComponentCategory;
+import com.google.appinventor.components.runtime.*;
+import rx.android.plugins.*;
+import rx.android.schedulers.*;
+import rx.android.*;
+import rx.functions.Action1;
+@DesignerComponent(
+        version = 1,
+        description = "Particle Text View",
+        category = ComponentCategory.EXTENSION,
+        nonVisible = true,
+        iconName = "")
+
+@SimpleObject(external = true)
+@UsesLibraries(libraries = "rx.jar, rxjava.jar")
+
+public class ParticleTextViewAix extends AndroidNonvisibleComponent {
+
+    //Activity and Context
+    private Context context;
+    private Activity activity;
+    private ParticleTextView particleTextView;
+
+    public ParticleTextViewAix(ComponentContainer container){
+        super(container.$form());
+        this.activity = container.$context();
+        this.context = container.$context();
+    }
+
+    @SimpleFunction()
+    public void Configure(AndroidViewComponent androidViewComponent, int rowStep, int columnStep, String targetText, double releasing, float particleRadius, double miniDistance, int textSize, String movingStrategy){
+FrameLayout frameLayout = (FrameLayout) androidViewComponent.getView();
+ParticleTextView particleTextView = new ParticleTextView(this.context); 
+RandomMovingStrategy randomMovingStrategy = new RandomMovingStrategy();
+ParticleTextViewConfig config = new ParticleTextViewConfig.Builder()
+                .setRowStep(rowStep)
+                .setColumnStep(columnStep)
+                .setTargetText(targetText)
+                .setReleasing(releasing)
+                .setParticleRadius(particleRadius)
+                .setMiniDistance(miniDistance)
+                .setTextSize(textSize)
+                .setMovingStrategy(randomMovingStrategy)
+                //.setParticleColorArray(String[] particleColorArray)
+                .instance();
+particleTextView.setConfig(config);
+frameLayout.addView(particleTextView, new FrameLayout.LayoutParams(-1, -1));
+    }
+@SimpleFunction()
+public void StartAnimation() {
+    particleTextView.startAnimation();
+    AnimationStarted();
+}
+@SimpleFunction()
+public void StopAnimation() {
+    particleTextView.stopAnimation();
+    AnimationStopped();
+
+}
+    @SimpleEvent()
+    public void AnimationStarted(){
+        EventDispatcher.dispatchEvent(this, "Animation Started");
+    }
+    @SimpleEvent()
+    public void AnimationStopped(){
+        EventDispatcher.dispatchEvent(this, "Animation Stopped");
+    }
+}
